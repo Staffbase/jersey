a167dd5bb3f955f7cf9311cb3be89509725dac11
andrelanka <mail@dr-lanka.de>
2017-10-15 14:01:41 +0200

    Closed memory leak from as described in https://github.com/jersey/jersey/issues/3259

--- core-common/src/main/java/org/glassfish/jersey/message/internal/Utils.java
+++ core-common/src/main/java/org/glassfish/jersey/message/internal/Utils.java
@@ -72,7 +72,7 @@ public final class Utils {
     public static File createTempFile() throws IOException {
         final File file = File.createTempFile("rep", "tmp");
         // Make sure the file is deleted when JVM is shutdown at last.
-        file.deleteOnExit();
+        //file.deleteOnExit(); Patched: Avoid memory leaks
         return file;
     }
 
